7251882ac8c5de8a4d74aac62d2c1bff1bb30e10
shafiq <ssekittoshafiq@gmail.com>
2025-01-12 22:03:34 +0300

    new

--- stahiza/asgi.py
+++ stahiza/asgi.py
@@ -1,16 +1,8 @@
+
 import os
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import stahiza.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stahiza.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            stahiza.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_asgi_application()
